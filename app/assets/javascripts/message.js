6159e0c0a5c60b6b9184b096be4c04777c92927e
kazu-tech <56834808+kazu-tech@users.noreply.github.com>
2019-11-04 15:33:33 +0900

    三項演算に変更

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function(){
   function buildHTML(message){ //_message.html.hamlからクラスを引っ張ってくる
-    if ( message.image) {
-      var html = 
+    var img = (message.image !== null)? `<img src=${message.image}>`:''
+    var html = 
         `<div class="message" data-message-id=${message.id}>
           <div class="upper-message">
             <div class="upper-message__user-name">
@@ -16,29 +16,11 @@ $(function(){
               ${ message.content }
             </p>
         </div>
-        <img src=${message.image} >
+        ${img}
       </div>`
     return html;
-  } else {
-    var html =
-    `<div class="message" data-message-id=${message.id}>
-            <div class="upper-message">
-              <div class="upper-message__user-name">
-                ${message.user_name}
-              </div>
-              <div class="upper-message__date">
-                ${message.date}
-              </div>
-            </div>
-            <div class="lower-message">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
-            </div>
-          </div>`
-        return html;
-      };
-  }
+  };
+  
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();　//メッセージが送信されたらイベント発火。この行でデフォルトを止める
