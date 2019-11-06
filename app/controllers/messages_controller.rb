8a62a885dd013c0048afe0dc4673fa0e4396baa4
kazu-tech <56834808+kazu-tech@users.noreply.github.com>
2019-11-06 12:41:40 +0900

    [WIP]自動更新

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -15,7 +15,7 @@ class MessagesController < ApplicationController
         format.json
       end
     else
-      @message = @group.message.includes(:user)
+      @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。' 
       render :index
     end
