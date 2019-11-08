65f26568623a4d5733776310035dea33d1af4536
kazu-tech <56834808+kazu-tech@users.noreply.github.com>
2019-11-08 14:56:55 +0900

    日時日本時間と非同期通信

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -36,10 +36,11 @@ $(document).on("turbolinks:load",function(){
       contentType: false,  //リクエストに含まれているdataの実際の型を変更しないための記述
     })
     
-    .done(function(message) {　//ajaxのリクエストが通ったらここに来る
-      var html = buildHTML(message);　//非同期でメッセージを送る時
-      $('.message').append(html);　//作成したHTMLをメッセージ画面の一番下に追加する {append()の使い方【 対象要素.append( 追加したい要素 ) 】}
-      $('.form__submit').prop('disabled', false);　//送信ボタンを有効にする
+    .done(function(message) { //ajaxのリクエストが通ったらここに来る
+      console.log(message)
+      var html = buildHTML(message); //非同期でメッセージを送る時
+      $('.messages').append(html); //作成したHTMLをメッセージ画面の一番下に追加する {append()の使い方【 対象要素.append( 追加したい要素 ) 】}
+      $('.form__submit').prop('disabled', false); //送信ボタンを有効にする
       $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight},'fast');
       $('form')[0].reset();
     })
@@ -54,6 +55,7 @@ $(document).on("turbolinks:load",function(){
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     if (window.location.href.match(/\/groups\/\d+\/messages/)){
       last_message_id = $('.message:last').data("message-id");
+      console.log(last_message_id)
       $.ajax({
         //ルーティングで設定した通りのURLを指定
         url: "api/messages",
